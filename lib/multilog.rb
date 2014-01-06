8333a8c0b6cb1187cf6674f3599bb37f0561c0d7
Steve Wattam <stephenwattam@gmail.com>
2014-01-06 12:50:03 +0000

    Code tidying using rubocop

--- lib/multilog.rb
+++ lib/multilog.rb
@@ -18,10 +18,10 @@ class MultiLog < Logger
   DEFAULT_LEVEL = Logger::UNKNOWN
 
   # Create a simple log object with one log level and one device
-  # 
+  #
   # The 'logdevs' argument takes the same values as specified in #configure_logs
   #
-  def initialize(logdevs = {}, progname = nil, shift_age = 0, shift_size = 1048576)
+  def initialize(logdevs = {}, progname = nil, shift_age = 0, shift_size = 1_048_576)
     super(nil, shift_age, shift_size)
     @progname     = progname
     @shift_age    = shift_age
@@ -41,15 +41,15 @@ class MultiLog < Logger
   #
   def configure_logs(logdevs = {})
     # Remove all exsiting logs
-    @logdevs.each{ |name, ld| remove_log(name) } if @logdevs
+    @logdevs.each { |name, ld| remove_log(name) } if @logdevs
 
     # Parse logdevs hash options
     @logdevs      = {}
     logdevs       = [logdevs] if logdevs.is_a? Hash
 
     # If the user provides a device then set up a single log as :log
-    unless logdevs.is_a? Array then
-      @logdevs[:default]    = {:dev => logdevs, :level => DEFAULT_LEVEL}
+    unless logdevs.is_a? Array
+      @logdevs[:default]    = {dev: logdevs, level: DEFAULT_LEVEL}
       @lowest_level         = @logdevs[:default][:level]
       return
     end
@@ -61,7 +61,7 @@ class MultiLog < Logger
       level       = ld[:level]        ||= DEFAULT_LEVEL
       shift_age   = ld[:shift_age]    ||= @shift_age
       shift_size  = ld[:shift_size]   ||= @shift_size
-      level       = MultiLog.string_to_level(level) unless level.is_a? Fixnum 
+      level       = MultiLog.string_to_level(level) unless level.is_a? Fixnum
 
       # Add to the name deely.
       add_log(name, dev, level, shift_age, shift_size)
@@ -76,16 +76,16 @@ class MultiLog < Logger
   # shift_age :: As per ruby's Logger class
   # shift_size :: As per ruby's Logger class
   #
-  def add_log(name, destination, level, shift_age = 0, shift_size = 1048576)
+  def add_log(name, destination, level, shift_age = 0, shift_size = 1_048_576)
     dev = LogDevice.new(destination, :shift_age => shift_age, :shift_size => shift_size)
 
-    @logdevs[name] = {:dev => dev, :level => level}
-    @lowest_level = level if (not @lowest_level) or level < @lowest_level
+    @logdevs[name] = {dev: dev, level: level}
+    @lowest_level = level if !@lowest_level || level < @lowest_level
   end
 
   # Stop logging to one of the logs
   def remove_log(name)
-    if(@logdevs[name])
+    if @logdevs[name]
       # Back up old level
       old_level = @logdevs[name][:level]
 
@@ -93,22 +93,22 @@ class MultiLog < Logger
       @logdevs.delete(name)
 
       # Update lowest level if we need to
-      @lowest_level = @logdevs.values.map{|x| x[:level] }.min if old_level == @lowest_level
+      @lowest_level = @logdevs.values.map { |x| x[:level] }.min if old_level == @lowest_level
     end
   end
 
   # Print a summary of log output devices to the log, at such a level
   # that all logs see it.
   def summarise
-    add(@lowest_level, "Summary of logs:")
-    if(@logdevs.length > 0)
+    add(@lowest_level, 'Summary of logs:')
+    unless @logdevs.empty?
       c = 0
-      @logdevs.each{|name, ld|
+      @logdevs.each do |name, ld|
         msg = " (#{c+=1}/#{@logdevs.length}) #{name} (level: #{MultiLog.level_to_string(ld[:level])}, device: fd=#{ld[:dev].dev.fileno}#{ld[:dev].dev.tty? ? " TTY" : ""}#{ld[:dev].filename ? " filename=#{ld[:dev].filename}" : ""})"
         add(@lowest_level,  msg)
-      } 
+      end
     else
-      add(@lowest_level, " *** No logs!") # Amusingly, this can never output
+      add(@lowest_level, ' *** No logs!') # Amusingly, this can never output
     end
   end
 
@@ -117,7 +117,7 @@ class MultiLog < Logger
     # Default
     unless level then
       level = name
-      name  = nil 
+      name  = nil
     end
 
     # Look up the level if the user provided a :symbol or "string"
@@ -129,7 +129,7 @@ class MultiLog < Logger
       @logdevs[name][:level] = level
     else
       # Set them all by default 
-      @logdevs.each{|name, logdev| logdev[:level] = level }
+      @logdevs.each{|_, logdev| logdev[:level] = level }
     end
   end
 
@@ -145,7 +145,7 @@ class MultiLog < Logger
     severity ||= UNKNOWN
 
     # give up if no logdevs or if too low a severity
-    return true if severity < @lowest_level or (not @logdevs.values.map{ |ld| ld[:dev].nil? }.include?(false))
+    return true if severity < @lowest_level || !(@logdevs.values.map { |ld| ld[:dev].nil? }.include?(false))
 
     # Set progname to nil unless it is explicitly specified
     progname ||= @progname
@@ -169,9 +169,9 @@ class MultiLog < Logger
   # Convert a log level to a string.
   #
   # Used in the output summary
-  def self.level_to_string(lvl) 
+  def self.level_to_string(lvl)
     labels = %w(DEBUG INFO WARN ERROR FATAL)
-    return labels[lvl] || "UNKNOWN"
+    return labels[lvl] || 'UNKNOWN'
   end
 
   # Convert a string to a logger level number.
